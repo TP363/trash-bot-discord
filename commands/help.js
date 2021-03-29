ec90f598301db7db351090f39dd4611b01cf1a8a
TP363 <80291244+TP363@users.noreply.github.com>
2021-03-29 12:15:58 +0430

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const { DiscordAPIError, MessageEmbed } = require("discord.js")
+
+exports.run = async (bot,message,args) => {
+
+    let embed = new MessageEmbed()
+
+    .setTitle('***trash bot lollllll!!!!***')
+    .setDescription('***hi im a trash bot i have no commands now that you can use bot my Developer will add some commands to me LOLOOOLLLOLOLO!!!!! ***')
+    .setColor("#11d4cf")
+    .setThumbnail(bot.user.avatarURL());
+
+    message.channel.send(embed)
+}
+exports.help = {
+name: 'help'
+}
